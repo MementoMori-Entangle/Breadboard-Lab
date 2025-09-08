db3021674714400758949cbcd7b385b852554f51
Entangle <vm.game.jp.002@gmail.com>
2025-09-08 20:58:04 +0900

    データ登録をサブスレッド化

--- pico_w_data_pool/bme280_output.py
+++ pico_w_data_pool/bme280_output.py
@@ -5,8 +5,13 @@ import web_server
 from data_pool import add_data
 from machine import I2C, Pin
 
+# センサーデータ用キュー
+sensor_queue = []
+queue_lock = _thread.allocate_lock()
+
 SEA_LEVEL_PRESSURE = 1011.1
 API_ELEVATION = 25
+SENSOR_INTERVAL_MS = 5000
 
 i2c = I2C(0, scl=Pin(1), sda=Pin(0))
 bme = bme280.BME280(i2c=i2c)
@@ -25,7 +30,7 @@ def sensor_loop():
         current_altitude = altitude_sea_level - API_ELEVATION
 
         print("Pressure: {:.2f} hPa, Altitude (気圧計): {:.2f} m, API標高: {:.2f} m, 現在高度: {:.2f} m, Temp: {:.2f} C".format(
-        pressure_hpa, altitude_sea_level, API_ELEVATION, current_altitude, temperature_c))
+            pressure_hpa, altitude_sea_level, API_ELEVATION, current_altitude, temperature_c))
 
         sensor_data = {
             'temperature': temperature_c,
@@ -34,11 +39,28 @@ def sensor_loop():
             'altitude_sea_level': altitude_sea_level,
             'current_altitude': current_altitude
         }
-        add_data(sensor_data)
-        utime.sleep_ms(5000)
+        # キューに追加
+        queue_lock.acquire()
+        sensor_queue.append(sensor_data)
+        queue_lock.release()
+        utime.sleep_ms(SENSOR_INTERVAL_MS)
+
+# サブスレッド：キューからデータを取り出してadd_data
+def data_pool_loop():
+    while True:
+        queue_lock.acquire()
+        if sensor_queue:
+            data = sensor_queue.pop(0)
+            queue_lock.release()
+            add_data(data)
+        else:
+            queue_lock.release()
+        utime.sleep_ms(SENSOR_INTERVAL_MS - 300)  # キュー監視間隔
 
 # Webサーバーを別スレッドで起動
 _thread.start_new_thread(web_server.run, ())
+# データプール処理を別スレッドで起動
+_thread.start_new_thread(data_pool_loop, ())
 # Wi-Fi + Webサーバー起動待機
 utime.sleep_ms(7000)
 # メインでセンサーループ
